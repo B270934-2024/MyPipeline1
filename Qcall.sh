d063267bc44ca7d166bd23e1b5632b7e100b2bc5
Matt Carre <s2761220@bioinfmsc5.ed.ac.uk>
2024-10-14 01:41:39 +0100

    Well, its working! Almost! Just need to sort out the fact its not finging the .sorted.bam files

--- Qcall.sh
+++ Qcall.sh
@@ -23,9 +23,9 @@ for Files in ${FileList[@]}; do
         fastqc -o $path/Output $Files -q
         FileJustName=$(echo "${Files}"|sed 's/\.fq\.gz$//')
         echo ${FileJustName}
-        cd $path/Output
+        cd ${path}/Output
         unzip -qqo ${FileJustName}_fastqc.zip
-	cd $path/Output/${FileJustName}_fastqc
+	cd ${path}/Output/${FileJustName}_fastqc
 	awk -v fjn=$FileJustName 'BEGIN {FS = "\t"; OFS = "\t";}
         {if($1=="FAIL"){print fjn,$1,$2;}}' summary.txt >> $path/Output/results.tsv
         awk -v fjn=$FileJustName '
@@ -35,18 +35,21 @@ for Files in ${FileList[@]}; do
         {if($1==">>Sequence"){print fjn,$1,$2,$3,$4,"\t",$5}}
         {if($1==">>Overrepresented"){print fjn,$1,$2,"\t",$3}}' fastqc_data.txt >>$path/Output/results.tsv
         awk -v fjn=$FileJustName '{if($NF=="fail"){print "\n",fjn,"Fails a test! Check results.\n Test failed is:",$0}}' fastqc_data.txt
-	awk -v fjn=$FileJustName '{if($NF=="fail"){count++}; {if(count>=4){print "We recommend deletion of ",fjn,"as it has failed ",count," tests."}}' fastqc_data.txt
+	awk -v fjn=$FileJustName 'BEGIN {count=0} {if($NF=="fail"){count++}} END
+       	{if(count>=4){print "We recommend deletion of ",fjn,"as it has failed ",count," tests."}}' fastqc_data.txt
         touch flag
-	awk -v fjn=$FileJustName '{if($NF=="fail"){count++}; {if(count>=4){print "Del"}}' fastqc_data.txt > flag
-	if [ $(wc -l < flag) >=1 ] 
+	echo "del" > flag
+	awk -v fjn=$FileJustName 'BEGIN {count=0} {if($NF=="fail"){count++}}END {if(count>=4){print "Del\nDel\nDel"}}' fastqc_data.txt > flag
+	wc -l < flag
+	if [ $(wc -l < flag) -gt 1 ] 
 	then
-           	cd $path
+         	cd ${path}
 		rm -i $Files
         fi
-        cd $path/Output
+        cd ${path}/Output
 	rm -rf ${FileJustName}_fastqc 
-        rm -f *.html
-        rm -f flag
+        rm -rf *.html
+        rm -rf flag
      	cd $path
 done
 
@@ -58,12 +61,13 @@ elif [ $pairing == "p" ]
 		echo Processing $Files
                 ###let the user know we are looking at this one file first.
 		FileJustName=$(echo "${Files}"|sed 's/\_1\.fq\.gz$//')
-		fastqc -o $path/Output $FileJustName_1.fq.gz $FileJustName_2.fq.gz -q
+		fastqc -o $path/Output ${FileJustName}_1.fq.gz ${FileJustName}_2.fq.gz -q
 		echo ${FileJustName}
-		cd $path/Output
-       		unzip -qqo ${FileJustName}_1_fastqc.zip
+		cd ${path}/Output
+       		echo ${path}/Output
+		unzip -qqo ${FileJustName}_1_fastqc.zip
 		unzip -qqo ${FileJustName}_2_fastqc.zip
-        	cd $path/Output/${FileJustName}_2_fastqc
+        	cd ${path}/Output/${FileJustName}_2_fastqc
         	awk -v fjn=$FileJustName 'BEGIN {FS = "\t"; OFS = "\t"}	{if($1=="FAIL"){print fjn,$1,$2;}}' summary.txt >> $path/results.tsv
         	awk -v fjn=$FileJustNam unzip -qqo ${FileJustName}_1_fastqc.zip
                 unzip -qqo ${FileJustName}_2_fastqc.zip
@@ -72,23 +76,24 @@ elif [ $pairing == "p" ]
         	{if($1=="Sequences" && $2=="flagged"){print fjn,$4,$5,"\t",$6}}
         	{if($1==">>Per"){print fjn,$1,$2,$3,$4,"\t",$5}}
         	{if($1==">>Sequence"){print fjn,$1,$2,$3,$4,"\t",$5}}
-        	{if($1==">>Overrepresented"){print fjn,$1,$2,"\t",$3}}' fastqc_data.txt >>$path/Output/results.tsv
+        	{if($1==">>Overrepresented"){print fjn,$1,$2,"\t",$3}}' fastqc_data.txt >>${path}/Output/results.tsv
         	awk -v fjn=$FileJustName '{if($NF=="fail"){print "\n",fjn,"Fails a test! Check results.\n Test failed is:",$0}}' fastqc_data.txt
-        	awk -v fjn=$FileJustName '{if($NF=="fail"){count++}; {if(count>=4){print "We recommend deletion of ",fjn,"as it has failed ",count," tests."}}' fastqc_data.txt
+        	awk -v fjn=$FileJustName 'BEGIN{count=0}{if($NF=="fail"){count++}} {if(count>=4){print "We recommend deletion of ",fjn,"as it has failed ",count," tests."}}' fastqc_data.txt
         	touch flag
-        	awk -v fjn=$FileJustName '{if($NF=="fail"){count++}; {if(count>=4){print "Del"}}' fastqc_data.txt > flag
-        	if [ $(wc -l < flag) >=1 ]
+		echo "del" > flag
+       		awk -v fjn=$FileJustName 'BEGIN {count=0} {if($NF=="fail"){count++}}END {if(count>=4){print "Del\nDel\nDel"}}' fastqc_data.txt > flag
+	        if [ $(wc -l < flag) -gt 1 ]
 	       	then
-               		cd $path
-                	rm -i $files
+               		cd ${path}
+                	rm -i ${files}
         	fi
-        	cd $path/Output
+        	cd ${path}/Output
         	rm -rf ${FileJustName}_fastqc
-	        rm -f *.html
-       		rm -f flag
+	        rm -rf *.html
+       		rm -rf flag
         	echo ${FileJustName}
-        	cd $path/Output
-        	cd $path/Output/${FileJustName}_2_fastqc
+        	cd ${path}/Output
+        	cd ${path}/Output/${FileJustName}_2_fastqc
         	awk -v fjn=$FileJustName 'BEGIN {FS = "\t"; OFS = "\t"} {if($1=="FAIL"){print fjn,$1,$2;}}' summary.txt >> $path/results.tsv
 	        awk -v fjn=$FileJustName '
 	        {if($1==">>Basic"){print fjn,$1,$2,"\t",$3;}}
@@ -97,24 +102,20 @@ elif [ $pairing == "p" ]
         	{if($1==">>Sequence"){print fjn,$1,$2,$3,$4,"\t",$5}}
         	{if($1==">>Overrepresented"){print fjn,$1,$2,"\t",$3}}' fastqc_data.txt >>$path/Output/results.tsv
         	awk -v fjn=$FileJustName '{if($NF=="fail"){print "\n",fjn,"Fails a test! Check results.\n Test failed is:",$0}}' fastqc_data.txt
-        	awk -v fjn=$FileJustName '{if($NF=="fail"){count++}; {if(count>=4){print "We recommend deletion of ",fjn,"as it has failed ",count," tests."}}' fastqc_data.txt
+        	awk -v fjn=$FileJustName 'BEGIN {count=0}{if($NF=="fail"){count++}} END {if(count>= 4){print "We recommend deletion of ",fjn,"as it has failed ",count," tests."}}' fastqc_data.txt
         	touch flag
-        	awk -v fjn=$FileJustName '{if($NF=="fail"){count++}; {if(count>=4){print "Del"}}' fastqc_data.txt > flag
-		if [ $(wc -l < flag) >=1 ]
+		echo "del" > flag
+		awk -v fjn="$FileJustName" 'BEGIN {count=0} {if($NF=="fail"){count++}} END {if(count>=4){print "Del\nDel\nDel"}}' fastqc_data.txt > flag
+		if [ $(wc -l < flag) -gt 1 ]
 	       	then
-                	cd $path
+               		cd $path
                  	rm -i $files
         	fi
         	cd $path/Output
        		rm -rf ${FileJustName}_fastqc
-        	rm -f *.html
-        	rm -f flag
+        	rm -rf *.html
+        	rm -rf flag
         	cd $path
 done
 fi
-
-
-
-
-
 echo -e "\n\n\n All files now processed! View results.txt in your FASTA folder. For more details, the fastqc_data.txt file, or the fastqc_report.html file."
